6030a4ebccbb9e85d2ecbb27b4edc591ca88d05a
Gökhan ZER <martperisi@gmail.com>
2021-01-24 19:32:25 +0300

    ikinci commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//toaster
+import { ToastrService } from 'ngx-toastr';
+//
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit {
+
   title = 'gogoTAngularest1';
+  constructor(private toastr: ToastrService) {}
+
+  ngOnInit() : void {
+    this.showSuccess();
+  }
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
 }
+
+
+
